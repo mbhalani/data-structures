577a91e5def7622fd82351952fb8fdf913c4d6a5
Mukesh B <mbhalani3@gmail.com>
2017-02-16 14:59:57 -0800

    implimented fn-prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueInstance = Object.create(queueMethods);
+
+  queueInstance.storage = {};
+  queueInstance.head = 0;
+  queueInstance.tail = 0;
+
+  return queueInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.tail += 1;
+  this.storage[this.tail] = value;
+};
 
+queueMethods.dequeue = function(value) {
+  let result;
+
+  if (this.head < this.tail) {
+    this.head += 1;
+    result = this.storage[this.head];
+    delete this.storage[this.head];
+  }
+
+  return result;
+};
+
+queueMethods.size = function(value) {
+  return this.tail - this.head;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackInstance = Object.create(stackMethods);
+
+  stackInstance.storage = {};
+  stackInstance._size = 0;
+
+  return stackInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this._size += 1;
+  this.storage[this._size] = value;
+};
 
+stackMethods.pop = function(value) {
+  let result;
+
+  if (this._size > 0) {
+    result = this.storage[this._size];
+    delete this.storage[this._size];
+    this._size -= 1;
+  }
+
+  return result;
+};
+
+stackMethods.size = function(value) {
+  return this._size;
+};
